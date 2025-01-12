e4ddae4f1e065dd04c70f1ed6ede1c5c434758a4
mariobarrosmbn <166873448+mariobarrosmbn@users.noreply.github.com>
2025-01-12 12:24:40 -0300

    lint code with rubocop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,16 @@
 def bubble_sort(arr)
-    n = arr.length
-    temp = 0
-    n.times do |i|
-        (n-1).times do |j|
-            if arr[j] > arr[j+1]
-                temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
-            end
-        end
+  n = arr.length
+  temp = 0
+  n.times do |_i|
+    (n - 1).times do |j|
+      next unless arr[j] > arr[j + 1]
+
+      temp = arr[j]
+      arr[j] = arr[j + 1]
+      arr[j + 1] = temp
     end
-    p arr
+  end
+  p arr
 end
 
 # numbers = [4,3,78,2,0,2]
